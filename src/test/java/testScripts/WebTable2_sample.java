322e7eeee467267baceb8f0136a9863ccb62fe3b
sheetal1612 <chaudharysheetal74@gmail.com>
2025-03-27 13:55:39 +0530

    Committing to phase2 branch

---
+++ src/test/java/testScripts/WebTable2_sample.java
@@ -0,0 +1,28 @@
+package testScripts;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WebTable2_sample {
+  @Test
+  public void search() {
+	  WebDriver driver= new ChromeDriver();
+	  driver.get("https://testautomationpractice.blogspot.com/");
+	  List<WebElement> eles= driver.findElements(By.xpath("//td[contains(text(),'Master In Selenium')]//following-sibling::td"));
+	  System.out.append("Details are: ");
+	  for(WebElement ele:eles)
+	  {
+		  System.out.append(ele.getText());
+	  }
+	  //select a book whose price is 2000
+	  
+	  WebElement eleBook= driver.findElement(By.xpath("//td[contains(text(),'2000')]//preceding-sibling::td[3]"));
+	  System.out.append("Book Name is "+eleBook.getText());
+	  
+  }
+}
